e54825699756f8bdae780c44ba65df6c73667be3
DoctorG13 <142879267+DoctorG13@users.noreply.github.com>
2026-09-05 23:29:58 +1000

    fix: load provider after production authentication

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import { ChrysalisProvider } from "./context/ChrysalisProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
-            <ChrysalisProvider>
-                <App />
-            </ChrysalisProvider>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
